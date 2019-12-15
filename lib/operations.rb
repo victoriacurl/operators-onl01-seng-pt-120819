a31a02be58e38a86edf4e40df6270f05d03ea90a
Victoria Curl <victoriacurl9@gmail.com>
2019-12-15 17:16:35 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,5 +1,5 @@
 def unsafe?(speed)
-if speed <40 || speed >60
+if speed < 40 || speed > 60
   true
 else
   false
@@ -9,6 +9,6 @@ end
 
 
 def not_safe?(speed)
-if speed >60 || speed <40 ? "true" : "false"
+if speed < 40 || speed > 60  ? "true" : "false"
 end
 end
